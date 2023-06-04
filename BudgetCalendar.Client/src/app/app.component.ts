77ad39697b58821b87b2307a93547e79fa786d44
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-06-04 02:59:06 +0100

    Re-Created Project as Angular 16

--- BudgetCalendar.Client/src/app/app.component.ts
+++ BudgetCalendar.Client/src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
-import { DayCardComponent } from './Features/Calendar/components/day-card/day-card.component';
-import { CalendarTableComponent } from './Features/Calendar/components/calendar-table/calendar-table.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NgSwitch, 
-      NgSwitchDefault, 
-      NgSwitchCase, 
-      RouterOutlet, 
-      DayCardComponent,
-      CalendarTableComponent]
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
 })
 export class AppComponent {
   title = 'BudgetCalendar.Client';
--- BudgetCalendar.Client/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://localhost:44379/api'
-}
