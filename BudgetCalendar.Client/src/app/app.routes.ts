94c6a07d623975841b0aae98d08038b8363739a2
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-06-12 20:27:02 +0100

    Created Auth base component/ "page" and auth.routes.ts

--- BudgetCalendar.Client/src/app/app.routes.ts
+++ BudgetCalendar.Client/src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const APP_ROUTES: Routes = [
   {
     path: 'calendar',
     loadChildren: () => import('./features/calendar/calendar.routes').then(m => m.CALENDAR_ROUTES)
-  }
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
 ];
