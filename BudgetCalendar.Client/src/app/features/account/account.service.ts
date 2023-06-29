fbdcc7d0bb1c2a9595450b3ff401a671ef3ff130
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-06-29 13:50:05 +0100

    Update account component with detailed table view
    The account component has been updated to display account information in a more detailed and structured table view.

---
+++ BudgetCalendar.Client/src/app/features/account/account.service.ts
@@ -0,0 +1,31 @@
+import {computed, inject, Injectable, Signal, signal} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { environment } from 'src/environments/environment.development';
+import {IAccount} from "./_interfaces/iAccount";
+import {toSignal} from "@angular/core/rxjs-interop";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  http = inject(HttpClient);
+  baseUrl = environment.baseUrl;
+
+  accounts = signal<IAccount[]>([])
+  totalBalance = computed(() => this.accounts()
+    .filter(account => account.balance)
+    .reduce((acc, account) => acc + account.balance!, 0));
+
+  constructor() {
+    this.getAccount()
+
+  }
+
+  getAccount() {
+    return this.http.get(`${this.baseUrl}/account`)
+      .subscribe((data: any) => {
+        this.accounts.set(data);
+      });
+  }
+}
