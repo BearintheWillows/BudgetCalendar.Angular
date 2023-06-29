0330afe651e6a354af253033b00dde45d2c2eed0
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-06-29 13:51:32 +0100

    Add account service and table module to account component
    New dependencies were added to the account component.

--- BudgetCalendar.Client/src/app/features/account/account.component.ts
+++ BudgetCalendar.Client/src/app/features/account/account.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, Signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AccountService} from "./account.service";
+import {TableModule} from "primeng/table";
+import {IAccount} from "./_interfaces/iAccount";
+
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent {
 
+  accountService = inject(AccountService);
+  accounts: Signal<IAccount[]> = computed(() => this.accountService.accounts());
+
+  ngOnInit() {
+  }
 }
