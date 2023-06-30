c4162f69aa9fc9bac9923a5f39149f5cc59c0ab8
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-06-30 23:42:02 +0100

    "Uncomment and update DB initialization logic"
    This commit updates the database initialization process in DataDbInitializer.cs. The previously commented-out code for initializing categories, accounts, and budget sequences has been uncommented and expanded to include the users. In addition, the 'UserId' attribute has also been added to respective entities. This change will populate the database with initial data, which is necessary for subsequent logic and testing efforts. Overall, the changes allow for a more comprehensive

--- BudgetCalendar.Server/Data/DataDbInitializer.cs
+++ BudgetCalendar.Server/Data/DataDbInitializer.cs
@@ -8,49 +8,129 @@ public static class DataDbInitializer
 {
 	public static void Initialize(DataDbContext context)
 	{
-		// if ( !context.Categories.Any() )
-		// {
-		// 	context.Categories.AddRange( GeneratCategories() );
-		// }
-		//
-		// if ( !context.Accounts.Any() )
-		// {
-		// 	
-		// 	context.Accounts.AddRange( GenerateAccounts() );
-		// }
-		// context.SaveChanges();
+		if ( !context.Categories.Any() )
+		{
+			context.Categories.AddRange( GeneratCategories() );
+		}
+
+		if ( !context.Accounts.Any() )
+		{
+
+			context.Accounts.AddRange( GenerateAccounts() );
+		}
+
+		if ( ! context.RecurringBudgetSequences.Any() )
+		{
+			context.RecurringBudgetSequences.AddRange( GenerateRBS() );
+		}
+
+		if ( !context.Budgets.Any() )
+		{
+			context.Budgets.AddRange( GenerateBudgets() );
+		}
+
+		context.SaveChanges();
 	}
 
-	// public static IEnumerable<Category> GeneratCategories()
-	// {
-	// 	return new List<Category>
-	// 		{
-	// 		new Category {Name = "Food" },
-	// 		new Category {Name = "Rent" },
-	// 		new Category {Name = "Utilities" },
-	// 		new Category {Name = "Entertainment" },
-	// 		new Category {Name = "Transportation" },
-	// 		new Category {Name = "Clothing" },
-	// 		new Category {Name = "Medical" },
-	// 		new Category {Name = "Insurance" },
-	// 		new Category {Name = "Personal" },
-	// 		new Category {Name = "Debt" },
-	// 		new Category {Name = "Education" },
-	// 		new Category {Name = "Savings" },
-	// 		new Category {Name = "Gifts" },
-	// 		new Category {Name = "Other" }
-	// 		};
-	// }
-	//
-	// public static IEnumerable<Account> GenerateAccounts()
-	// {
-	// 	return new List<Account>
-	// 		{
-	// 		new() { Name = "FlexPlus", Balance = 0, IsArchived = false, Added = DateTime.Now},
-	// 		new() { Name = "Monzo", Balance = 0, IsArchived = false, Added = DateTime.Now},
-	// 		};
-	// }
-	//
-	
+	public static IEnumerable<Category> GeneratCategories()
+	{
+		return new List<Category>
+			{
+			new Category { Name = "Food", UserId = $"67dc3800-b214-4eeb-aa21-a3b4526deee1" },
+			new Category { Name = "Rent", UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1" },
+			};
+	}
 
+	public static IEnumerable<Account> GenerateAccounts()
+	{
+		return new List<Account>
+			{
+			new()
+				{
+				Name = "FlexPlus",
+				Balance = 0,
+				IsArchived = false,
+				Added = DateTime.Now,
+				UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1"
+				},
+			new()
+				{
+				Name = "Monzo",
+				Balance = 0,
+				IsArchived = false,
+				Added = DateTime.Now,
+				UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1"
+				},
+			};
+	}
+
+	public static IEnumerable<RecurringBudgetSequence> GenerateRBS()
+	{
+		return new List<RecurringBudgetSequence>()
+			{
+			new(new DateTime( 2023, 06, 23 ), null, RecurringBudgetInterval.Monthly),
+			
+			};
+	}
+
+	public static IEnumerable<Budget> GenerateBudgets()
+	{
+		return new List<Budget>()
+			{
+			new()
+				{
+				Amount = 0,
+				Date = new DateTime( 2023, 06, 23 ),
+				IsArchived = false,
+				TransactionType = TransactionType.Income,
+				Note = "Test",
+				Color = "Test",
+				AccountId = 1,
+				CategoryId = 3,
+				RecurringBudgetSequenceId = 2,
+				UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1"
+				},
+			new()
+				{
+				Amount = 100,
+				Date = new DateTime( 2023 / 06 / 15 ),
+				IsArchived = false,
+				TransactionType = TransactionType.Expense,
+				Note = "Test",
+				Color = "Test",
+				AccountId = 1,
+				CategoryId = 4,
+				RecurringBudgetSequenceId = 2,
+				UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1"
+				},
+			new()
+				{
+				Amount = 100,
+				Date = new DateTime( 2023 / 06 / 02 ),
+				IsArchived = false,
+				TransactionType = TransactionType.Income,
+				Note = "Test",
+				Color = "Test",
+				AccountId = 1,
+				CategoryId = 3,
+				RecurringBudgetSequenceId = 2,
+				UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1"
+				},
+			new()
+				{
+				Amount = 300,
+				Date = new DateTime( 2023 / 06 / 02 ),
+				IsArchived = false,
+				TransactionType = TransactionType.Expense,
+				Note = "Test",
+				Color = "Test",
+				AccountId = 2,
+				CategoryId = 4,
+				RecurringBudgetSequenceId = 2,
+				UserId = "67dc3800-b214-4eeb-aa21-a3b4526deee1"
+				},
+			};
+
+
+	}
 }
